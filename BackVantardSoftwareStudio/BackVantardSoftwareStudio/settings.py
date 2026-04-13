d3efaa54b57edfe2cfda9f006f222912c7478133
AmauriGC <20233tn102@utez.edu.mx>
2026-04-13 00:07:15 -0600

    feat(migraciones): Eliminacion de las migraciones

--- BackVantardSoftwareStudio/BackVantardSoftwareStudio/settings.py
+++ BackVantardSoftwareStudio/BackVantardSoftwareStudio/settings.py
@@ -160,10 +160,18 @@ WSGI_APPLICATION = 'BackVantardSoftwareStudio.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.parse(
-        config('MYSQL_PUBLIC_URL'),
-        conn_max_age=600,
-    )
+    'default': {
+        'ENGINE':   'django.db.backends.mysql',
+        'NAME':     config('DB_NAME'),
+        'USER':     config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST':     config('DB_HOST', default='localhost'),
+        'PORT':     config('DB_PORT', default='3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
 }
 
 AUTH_USER_MODEL = 'users.User'
