cbe3b84e2eff0887f0a6ea091396712ef0711317
AmauriGC <20233tn102@utez.edu.mx>
2026-04-13 01:32:58 -0600

    fix(base): cambio de base de datos

--- BackVantardSoftwareStudio/BackVantardSoftwareStudio/settings.py
+++ BackVantardSoftwareStudio/BackVantardSoftwareStudio/settings.py
@@ -160,18 +160,10 @@ WSGI_APPLICATION = 'BackVantardSoftwareStudio.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE':   'django.db.backends.mysql',
-        'NAME':     config('DB_NAME'),
-        'USER':     config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST':     config('DB_HOST', default='localhost'),
-        'PORT':     config('DB_PORT', default='3306'),
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
-    }
+    'default': dj_database_url.parse(
+        config('MYSQL_PUBLIC_URL'),
+        conn_max_age=600,
+    )
 }
 
 AUTH_USER_MODEL = 'users.User'
